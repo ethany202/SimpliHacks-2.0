d8e9d38f0a74038428a9d90d4f9a5500b0e33d5a
dhshin04 <dhshin0301@gmail.com>
2022-06-25 10:08:28 -0400

    Search Bar Added

--- static/js/home.js
+++ static/js/home.js
@@ -1,17 +1,31 @@
-for (let i = 0; i < data.length; i++) {
-    result[i].style.display = "none";
+function addEvent(muscleName) {
+    window.location.href="https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_trim"
+    console.log(muscleName);
 }
 
+var click = false;
 function clicked() {
-    const input = document.getElementById('search'); 
-    let result = document.getElementsByClassName('result');
-    let data = [].map.call(result, element => element.textContent);
+    click = true;
+}
 
-    input.addEventListener("click", () => {
+function released() {
+    if (click) {
+        let result = document.getElementsByClassName('result');
+        let data = [].map.call(result, element => element.textContent);
+
         for (let i = 0; i < data.length; i++) {
             result[i].style.display = "list-item";
         }
-    });
+        click = false;
+    }
+    else {
+        let result = document.getElementsByClassName('result');
+        let data = [].map.call(result, element => element.textContent);
+
+        for (let i = 0; i < data.length; i++) {
+            result[i].style.display = "none";
+        }
+    }
 }
 
 function user_search() {
@@ -21,6 +35,8 @@ function user_search() {
     let input_value = document.getElementById('search').value.toLowerCase();  
 
     for (let i = 0; i < data.length; i++) {
+        data[i].replace("\n", "");
+        data[i] = data[i].trim();
         if (data[i].substring(0, input_value.length).toLowerCase() !== input_value) {
             result[i].style.display = "none";
         }
